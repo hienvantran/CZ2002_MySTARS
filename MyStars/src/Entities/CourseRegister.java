14e3f805ceab23b6f9c3a60c4b114d9fd6e66ccc
TRAN HIEN VAN <54805500+hienvantran@users.noreply.github.com>
2020-11-08 10:58:27 +0800

    Update CourseRegister.java

--- MyStars/src/Entities/CourseRegister.java
+++ MyStars/src/Entities/CourseRegister.java
@@ -3,11 +3,11 @@ package Entities;
 public class CourseRegister {
 	
 	private Boolean status;
-	private Course course;
-	private Index index;
-	private Student student;
+	private String course;
+	private int index;
+	private String student;
 	
-	public CourseRegister(Student student, Index index, Course course, Boolean status)
+	public CourseRegister(String student, String course, int index,  Boolean status)
 	{
 		this.status = status;
 		this.student = student;
@@ -18,12 +18,12 @@ public class CourseRegister {
 	public Boolean getStatus() { return status;}
 	public void setStatus(Boolean status) { this.status = status;}
 	
-	public Course getCourse() { return course;}
-	public void setCourse(Course course) { this.course = course;}
+	public String getCourse() { return course;}
+	public void setCourse(String course) { this.course = course;}
 	
-	public Index getIndex() { return index;}
-	public void setIndex(Index index) { this.index = index;}
+	public int getIndex() { return index;}
+	public void setIndex(int index) { this.index = index;}
 	
-	public Student getStudent() { return student;}
-	public void setStudent(Student student) { this.student = student;}
+	public String getStudent() { return student;}
+	public void setStudent(String student) { this.student = student;}
 }
