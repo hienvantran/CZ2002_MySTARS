6fe8dc8bd80ce384632b49bea6be1f95cac45ee9
TRAN HIEN VAN <54805500+hienvantran@users.noreply.github.com>
2020-11-09 10:29:04 +0800

    Update IndexDB.java

--- MyStars/src/DB/IndexDB.java
+++ MyStars/src/DB/IndexDB.java
@@ -19,12 +19,14 @@ public class IndexDB {
 			// get individual 'fields' of the string separated by SEPARATOR
 			// pass in the string to the string tokenizer using delimiter "," 
 			StringTokenizer tokenizer = new StringTokenizer(field, SEPARATOR);	
-			//CourseCode/index/Group/waitList/vacancy
+			//CourseCode/index/Group/vacancy/waitList
 			String  crsCode = tokenizer.nextToken().trim();	
 			int index = Integer. parseInt((tokenizer.nextToken().trim())); 
 			String  group = tokenizer.nextToken().trim();
 			int vacancy = Integer. parseInt((tokenizer.nextToken().trim()));
 			int waitlist = Integer. parseInt((tokenizer.nextToken().trim()));
+			
+			
 			
 			// create Course object and add to course list 
 			Index idx = new Index( crsCode, index, group, vacancy, waitlist);
