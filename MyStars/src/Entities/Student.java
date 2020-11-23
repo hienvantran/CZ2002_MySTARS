9a1e35d61b24559fe3600ec43bfe94d9f9e3700a
jkrolling <65852623+jkrolling98@users.noreply.github.com>
2020-11-24 06:23:41 +0800

    added javadocs

--- MyStars/src/Entities/Student.java
+++ MyStars/src/Entities/Student.java
@@ -3,14 +3,50 @@ package Entities;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * Student class that extends user!
+ * @author Jia Kai
+ * @version 1.0
+ * @since 2020-11-21
+ */
 public class Student extends User{
+	/**
+	 * the matric number of student
+	 */
     private String matricNum;
+	/**
+	 * the nationality of student
+	 */
     private String nationality;
+	/**
+	 * the year of study of student
+	 */
     private int yearOfStudy;
+	/**
+	 * the email of student
+	 */
     private String email;
+	/**
+	 * the access start date time of student
+	 */
     private Calendar accessStart;
+    /**
+	 * the access end date time of student
+	 */
     private Calendar accessEnd;
 
+    /**
+     * Creates a new student with given parameters
+     * @param username
+     * @param password
+     * @param modeType
+     * @param matricNum
+     * @param nationality
+     * @param yearOfStudy
+     * @param email
+     * @param accessStart
+     * @param accessEnd
+     */
     public Student(String username, String password, ModeType modeType, String matricNum,String nationality,int yearOfStudy,String email, Calendar accessStart, Calendar accessEnd){
         super(username, password, modeType);
         this.matricNum = matricNum;
@@ -22,49 +58,98 @@ public class Student extends User{
 
     }
 
+    /**
+     * Gets the year of study of the student
+     * @return this student's year of study
+     */
     public int getYearOfStudy() {
         return yearOfStudy;
     }
 
+    /**
+     * changes the year of study of the student
+     * @param yearOfStudy
+     */
     public void setYearOfStudy(int yearOfStudy) {
         this.yearOfStudy = yearOfStudy;
     }
 
+    /**
+     * Gets the access start date time of the student
+     * @return this student's access start date time
+     */
     public Calendar getAccessStart() {
         return accessStart;
     }
 
+    /**
+     * changes the access start date time of the student
+     * @param accessStart
+     */
     public void setAccessStart(Calendar accessStart) {
         this.accessStart = accessStart;
     }
 
+    /**
+     * Gets the access end date time of the student
+     * @return this student's access end date time
+     */
     public Calendar getAccessEnd() {
         return accessEnd;
     }
 
+    /**
+     * changes the access end date time of the student
+     * @param accessEnd
+     */
     public void setAccessEnd(Calendar accessEnd) {
         this.accessEnd = accessEnd;
     }
 
+    /**
+     * Gets the nationality of the student
+     * @return this student's nationality
+     */
     public String getNationality() {
         return nationality;
     }
 
+    /**
+     * changes the nationality of the student
+     * @param nationality
+     */
     public void setNationality(String nationality) {
         this.nationality = nationality;
     }
 
+    /**
+     * Gets the matric number of the student
+     * @return this student's matric number
+     */
     public String getMatricNum() {
         return matricNum;
     }
 
+    /**
+     * changes the matric number of the student
+     * @param matricNum
+     */
     public void setMatricNum(String matricNum) {
         this.matricNum = matricNum;
     }
+    
+    /**
+     * Gets the email of the student
+     * @return this student's email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * changes the email of the student
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
--- MyStars/src/Entities/User.java
+++ MyStars/src/Entities/User.java
@@ -5,86 +5,66 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * an abstract User class
+ * @author Jia Kai
+ * @version 1.0
+ * @since 2020-11-21
+ */
 public class User{
-    private int id;
     private String username;
     private String password;
     private ModeType mode;
-    private String firstName;
-    private String lastName;
-    private String gender;
-    private String email;
-
-    public User(){
-
-    }
-
     public User(String username, String password, ModeType mode){
         this.username = username;
         this.password = password;
         this.mode = mode;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Gets the password of this user
+     * @return this user's password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Changes the password of this user
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
+    /**
+     * Gets the modetype of this user
+     * @return this user's modetype
+     */
     public ModeType getMode() {
         return mode;
     }
-
+    
+    /**
+     * Changes the modetype of this user
+     * @param mode
+     */
     public void setMode(ModeType mode) {
         this.mode = mode;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    /**
+     * Gets the username of this user
+     * @return this user's username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Changes the username of this user
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
