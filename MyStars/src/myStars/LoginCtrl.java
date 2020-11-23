34322556d64431253c8d4a6500d6ea99032248b3
jkrolling <65852623+jkrolling98@users.noreply.github.com>
2020-11-24 04:23:29 +0800

    added javadoc for loginctrl

--- MyStars/src/myStars/LoginCtrl.java
+++ MyStars/src/myStars/LoginCtrl.java
@@ -14,18 +14,42 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+/**
+ * Controller class for login
+ * @author Jia Kai
+ * @version 1.0
+ * @since 2020-11-23
+ */
 public class LoginCtrl {
+	/**
+	 * the username input of the login attempt
+	 */
 	private String username;
+	/**
+	 * the password input of the login attempt
+	 */
 	private String password;
+	/**
+	 * the domain of the login attempt
+	 */
 	private ModeType mode;
-	private boolean validated;
-	
+	/**
+	 * the validity of the login attempt
+	 * default value = false
+	 */
+	private boolean validated= false;
+	/**
+	 * constructor for Login Ctrl
+	 * @param accountType to define the logjn attempt
+	 */
 	public LoginCtrl(ModeType accountType) {
 		this.mode = accountType;
 		this.validated = false;
 	}
 	
+	/**
+	 * to input the username for the login attempt
+	 */
 	public void setUsername() {
 		System.out.println("Please enter your username: ");
 		Scanner sc = new Scanner(System.in);
@@ -33,6 +57,11 @@ public class LoginCtrl {
 		this.username = input;
 	}
 	
+	/**
+	 * to input the password for the login attempt
+	 * password will be masked during input (except for when using eclipse)
+	 * password input will also be hashed to compare with the hashed password in DB
+	 */
 	public void setPassword() {
 		String password;
 		System.out.println("Please enter your password: ");
@@ -40,6 +69,10 @@ public class LoginCtrl {
 		this.password= HashCtrl.hashPassword(password);
 	}
 	
+	/**
+	 * function to mask the input during password
+	 * @return input for password
+	 */
 	public String pass() {
 		java.io.Console c = System.console();
 		if (c == null) {
@@ -54,18 +87,35 @@ public class LoginCtrl {
 		return pass;
 	}
 	
+	/**
+	 * gets the username of the login attempt
+	 * @return username input
+	 */
 	public String getUsername() {
 		return this.username;
 	}
 	
+	/**
+	 * gets the password of the login attempt
+	 * @return password input
+	 */
 	public String getPassword() {
 		return this.password;
 	}
 	
+	/**
+	 * sets the validity of the login attempt to true
+	 */
 	public void makeValid() {
 		this.validated=true;
 	}
 	
+	/**
+	 * function to initiate a login attempt
+	 * @return true or false depending on whether attempt is successful
+	 * @throws ParseException
+	 * @throws FileNotFoundException
+	 */
 	public boolean login() throws ParseException , FileNotFoundException{
 		this.setUsername();
 		this.setPassword();
@@ -107,6 +157,11 @@ public class LoginCtrl {
 		
 	}
 	
+	/**
+	 * function call to verify that admin username input and password input matches the one in adminDB
+	 * @param user admin account 
+	 * @return true or false depending on validity of login attempt
+	 */
 	public boolean match(User user) {
     	if((this.getUsername().toLowerCase()).equals((user.getUsername().toLowerCase())) && this.getPassword().equals(user.getPassword())) {
     		return true;
@@ -114,6 +169,11 @@ public class LoginCtrl {
     	else return false;
     }
 	
+	/**
+	 * function call to verify that student username input and password input matches the one in studentDB
+	 * @param student student account 
+	 * @return true or false depending on validity of login attempt
+	 */
 	public boolean match(Student student) {
 //		System.out.println("lower case input = "+ this.getUsername().toLowerCase());
 //		System.out.println("Matched against = " + student.getUsername().toLowerCase());
